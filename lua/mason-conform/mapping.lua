40133b4bf3e0322e7983f20183a062ba4a6db122
Andreas Palm <andreaspalm98@gmail.com>
2024-06-13 18:25:51 +0200

    fix(mapping): Incorrect name for clang-format

--- lua/mason-conform/mapping.lua
+++ lua/mason-conform/mapping.lua
@@ -23,7 +23,7 @@ M.conform_to_package = {
     ["buf"] = "buf",
     ["buildifier"] = "buildifier",
     ["cbfmt"] = "cbfmt",
-    ["clang_format"] = "clang-format",
+    ["clang-format"] = "clang-format",
     -- cljstyle
     ["cmake_format"] = "cmakelang",
     ["codespell"] = "codespell",
